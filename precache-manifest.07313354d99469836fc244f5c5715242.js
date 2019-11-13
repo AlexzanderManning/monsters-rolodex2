ab438ac0dc7dc1631202dc34ddadf84edea1f62c
AlexzanderManning <deonte94@icloud.com>
2019-11-13 14:48:00 -0500

    Updates

---
+++ precache-manifest.07313354d99469836fc244f5c5715242.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383b330947d09a1101e5a2407e1aa225",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "16099725b70c6fe40236",
+    "url": "/monsters-rolodex/static/css/main.71c04879.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "16099725b70c6fe40236",
+    "url": "/monsters-rolodex/static/js/main.ecfb7849.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
